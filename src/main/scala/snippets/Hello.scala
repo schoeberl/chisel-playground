e8f08c68dee61374f3fb421aa2b2338923228684
Martin Schoeberl <martin@jopdesign.com>
2019-01-11 17:24:00 -0800

    Move a class from Chisel 3 play to this play-ground

---
+++ src/main/scala/snippets/Hello.scala
@@ -0,0 +1,33 @@
+/*
+ * Play ground for Chisel syntax checks.
+ * 
+ * Author: Martin Schoeberl (martin@jopdesign.com)
+ * 
+ */
+
+package snippets
+
+import chisel3._
+import chisel3.Driver
+
+/**
+ * The blinking LED component.
+ */
+
+class Hello extends Module {
+  val io = IO(new Bundle {
+    val iport = Input(UInt(8.W))
+    val oport = Output(UInt(8.W))
+  })
+
+  io.oport := io.iport & "hf0".U
+}
+
+/**
+ * An object containing a main() to generate the Verilog code.
+ */
+object Hello {
+  def main(args: Array[String]): Unit = {
+    chisel3.Driver.execute(Array[String](), () => new Hello())
+  }
+}
